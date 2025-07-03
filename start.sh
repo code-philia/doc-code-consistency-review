5813043c3d447b38b16a0252c04c6d1368e12ca4
HaHaKwy <1625827540@qq.com>
2025-07-03 20:18:09 +0800

    minor

--- start.sh
+++
@@ -1,3 +0,0 @@
-# 使用 LlamaFactory CLI 启动 API 服务，指定模型和模板
-CUDA_VISIBLE_DEVICES=0,2,3 API_PORT=8001 llamafactory-cli api \
-    --model_name_or_path /home/kwy/project/models/deepseek-coder-6.7b-instruct
